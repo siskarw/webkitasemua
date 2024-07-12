7e30bcbbcfcfbc6cae92a19baa754d739bec865f
siskarw <siskarahmawati2764@students.amikom.ac.id>
2024-07-12 22:18:29 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,44 @@
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+
+
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDAeXTiTB5WnEoxlFdub0IYydNaIOU_QmY",
+    authDomain: "login-aza.firebaseapp.com",
+    projectId: "login-aza",
+    storageBucket: "login-aza.appspot.com",
+    messagingSenderId: "175360617845",
+    appId: "1:175360617845:web:6c90e10c009b5fe722f73c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+
+const submit = document.getElementById('submit');
+submit.addEventListener("click", function (event) {
+    event.preventDefault()
+    const email = document.getElementById('typeEmailX-2').value;
+    const password = document.getElementById('typePasswordX-2').value;
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            alert("logging in")
+            window.location.href="home.html"
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage)
+            // ..
+        });
+
+})
